af3711e13b7f8e405cc3275f3967a07faba00172
vukp <pukvavlovic10@gmail.com>
2025-11-22 18:57:31 +0100

    Component now imports Header and InfoModal, and has a useState variable that determines if the modal is open or not. It passes props to the Header and InfoModal components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./styles/globals.css";
+import { useState } from "react";
 import Header from "./components/Header.jsx";
+import InfoModal from "./components/InfoModal.jsx";
 
 export default function App() {
+  const [infoOpen, setInfoOpen] = useState(false);
   return (
     <>
-      <Header />
+      <Header onInfoClick={() => setInfoOpen(true)} />
+
+      {infoOpen && <InfoModal onClose={() => setInfoOpen(false)} />}
     </>
   );
 }
