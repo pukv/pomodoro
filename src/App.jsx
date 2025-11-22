fbffc79ad7422531f5cfe09254d50d302dcfa323
vukp <pukvavlovic10@gmail.com>
2025-11-22 22:40:18 +0100

    imported the timer and displayed it

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/globals.css";
 import { useState } from "react";
 import Header from "./components/Header.jsx";
 import InfoModal from "./components/InfoModal.jsx";
+import PomodoroTimer from "./components/PomodoroTimer.jsx";
 
 export default function App() {
   const [infoOpen, setInfoOpen] = useState(false);
@@ -10,6 +11,7 @@ export default function App() {
       <Header onInfoClick={() => setInfoOpen(true)} />
 
       {infoOpen && <InfoModal onClose={() => setInfoOpen(false)} />}
+      <PomodoroTimer />
     </>
   );
 }
